da60b895bb3ed7ad9b20e749350c606ebe35cca6
Connor Carciofini <ccarciofini@gmail.com>
2023-04-05 18:59:15 -0500

    dirs typo

--- api/settings.py
+++ api/settings.py
@@ -152,7 +152,7 @@ STATIC_URL = 'static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, './client/build/static')
+    os.path.join(BASE_DIR, 'client/build/static')
 ]
 
 # Test Runner Config
