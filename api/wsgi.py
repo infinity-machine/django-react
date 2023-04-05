f3aa90d6202eadb0ee2c5c2b4de32dc450ef9e5e
Connor Carciofini <ccarciofini@gmail.com>
2023-04-05 18:57:18 -0500

    whitenoise in wsgi

--- api/wsgi.py
+++ api/wsgi.py
@@ -8,10 +8,12 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
-
+from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+
+
 application = get_wsgi_application()
+application = WhiteNoise(application)
