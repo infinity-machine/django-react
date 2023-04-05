4641a4ec2409d818102980c3d0279a91ada9de65
Connor Carciofini <ccarciofini@gmail.com>
2023-04-05 17:56:21 -0500

    tutorial

--- Procfile
+++ Procfile
@@ -1,4 +1,2 @@
-web: gunicorn backend.wsgi --log-file
-release: python manage.py makemigrations --noinput
-release: python manage.py collectstatic --noinput
-release: python manage.py migrate --noinput
+release: python manage.py migrate
+web: gunicorn api.wsgi --log-file -
--- api/settings.py
+++ api/settings.py
@@ -144,14 +144,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, './client/build/static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'client/build/static')
+# ]
 
 # Test Runner Config
 class HerokuDiscoverRunner(DiscoverRunner):
