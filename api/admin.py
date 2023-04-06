4f65a63584a5984f31158953f7b7e572ca5f3b88
Connor Carciofini <ccarciofini@gmail.com>
2023-04-06 01:57:43 -0500

    register

--- api/admin.py
+++ api/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from api.models import UserModel
 
 class UserAdmin(admin.ModelAdmin):
-    pass
+    fields=('id', 'name')
 
 admin.site.register(UserModel, UserAdmin)
